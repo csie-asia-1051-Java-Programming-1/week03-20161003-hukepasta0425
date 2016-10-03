945818737c4c4f57e0ba49f5148c5c35042ff050
hukepasta0425 <105021031@live.asia.edu.tw>
2016-10-03 15:39:35 +0800

    10/3
    hw

---
+++ src/hw/hw01_105021031.java
@@ -0,0 +1,13 @@
+package hw;
+import java.util.*;
+public class hw01_105021031 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner src=new Scanner(System.in);
+		String str=src.next();
+		int sum=str.length();
+		System.out.print(sum);
+	}
+
+}
